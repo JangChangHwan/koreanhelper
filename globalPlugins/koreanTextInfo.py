f1f216c45c183f65cbcdc042d44263b17a6f7389
JangChangHwan <462356@gmail.com>
2020-08-10 14:20:01 +0900

    editted statictext field reading

--- globalPlugins/koreanTextInfo.py
+++ globalPlugins/koreanTextInfo.py
@@ -3,14 +3,22 @@
 import globalPluginHandler
 import ui
 import textInfos
+import controlTypes
 from textInfos.offsets import *
+from NVDAObjects import NVDAObjectTextInfo
 from NVDAObjects.window.edit import Edit, EditTextInfo
+from NVDAObjects.IAccessible import IAccessible
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if obj.windowClassName == "Edit":
+		# 단순 편집창
+		if obj.role == controlTypes.ROLE_EDITABLETEXT and obj.windowClassName == "Edit":
 			clsList.insert(0, KoreanEdit)
+		# 고정 텍스트
+		elif obj.role == controlTypes.ROLE_STATICTEXT:
+			clsList.insert(0, KoreanStaticText)
+
 
 class KoreanEditTextInfo(EditTextInfo):
 	def _getWordOffsets(self, offset):
@@ -32,4 +40,19 @@ class KoreanEdit(Edit):
 		info._setCaretOffset(info._startOffset)
 		info.expand(unit=textInfos.UNIT_WORD)
 		ui.message(info.text)
+
+
+
+
+class KoreanNVDAObjectTextInfo(NVDAObjectTextInfo):
+	def _getWordOffsets(self, offset):
+		paraStart, paraEnd = self._getParagraphOffsets(offset)
+		paraText = self._getTextRange(paraStart, paraEnd)
+		start = findStartOfWord(paraText, offset - paraStart) + paraStart
+		end = findEndOfWord(paraText, offset - paraStart) + paraStart
+		return start, end
+
+
+class KoreanStaticText(IAccessible):
+	TextInfo = KoreanNVDAObjectTextInfo
 
